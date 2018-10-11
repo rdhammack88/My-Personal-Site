e9a89dda3d026366ca1ecd3efe7408944950c1dc
Dustin <rdhammack88@hotmail.com>
2018-10-11 04:44:29 -0500

    changed the extension for required file of contact

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require("./src/views/header.html");
 require("./src/views/home.html");
 require("./src/views/about.html");
 require("./src/views/projects.php");
-require("./src/views/contact.php");
+require("./src/views/contact.html");
 require("./src/views/footer.html");
 
 

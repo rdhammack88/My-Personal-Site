9acd3af231c77ca1cd16b2e8c4112133841cb46a
rdhammack88 <rdhammack88@hotmail.com>
2018-10-22 15:42:36 -0500

    Changed scrolling of window to seperate functions

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
-    function windowScroll() {
-        /**
-         * When window is scrolled past the top 50px, add class to navbar that changes nav colors
-         */
+    /**
+     * When window is scrolled past the top 50px, add class to navbar that changes nav colors
+     */
+    function fixNavbar() {
         if ($('.navbar').offset().top > 50) {
             $('.fixed-top').addClass('top-nav-scroll');
         } else {
             $('.fixed-top').removeClass('top-nav-scroll');
         }
+    }
 
-        /**
-         * Since Contact form is NOT full height of window, the scroll to feature doesn't work adding the 'active' class
-         * to the parent 'li'
-         * This checks the window height to verify if the contact form is in view,
-         * and adds the 'active' class to the 'contact-link' parent 'li'
-         */
+    /**
+     * Since Contact form is NOT full height of window, the scroll to feature doesn't work adding the 'active' class
+     * to the parent 'li'
+     * This checks the window height to verify if the contact form is in view,
+     * and adds the 'active' class to the 'contact-link' parent 'li'
+     */
+    function activateContactLink() {
         if (
             $(document).height() - $(window).height() <=
             $(window).scrollTop()
@@ -37,7 +39,78 @@ $(document).ready(function() {
         }
     }
 
+    /**
+     * When page scrolls to skills-container section,
+     * animate the progress of each skill.
+     * When page scrolls away from skills-container,
+     * set progress back to 0.
+     */
+    function animateProgress() {
+        /** if window is past skills section */
+        /** else if window is above skills section */
+        /** else if window is centered on skills section */
+        if (
+            $(window).scrollTop() >=
+            $('.skills-container').offset().top +
+                $('.skills-container').height()
+        ) {
+            $('.progress-ring circle:nth-of-type(2)').removeClass();
+        } else if (
+            $(window).scrollTop() <=
+            $('.about-wrapper').offset().top - $(window).height() / 2
+        ) {
+            $('.progress-ring circle:nth-of-type(2)').removeClass();
+            // } else if (
+            //     $('.skills-container').offset().top -
+            //         $(window).height() +
+            //         $('.skills-container').height() / 2 <=
+            //     $(window).scrollTop()
+            // ) {
+        } else {
+            animatedClasses = [
+                'html-progress-ring',
+                'css-progress-ring',
+                'javascript-progress-ring',
+                'php-progress-ring'
+            ];
+            $('.progress-ring circle:nth-of-type(2)').each(function(i) {
+                $(this).addClass(animatedClasses[i]);
+            });
+            // $('.skills-container').show();
+            $('.skills-container').addClass('animateFadeIn');
+            // .css({
+            //     animation: 'fadeIn 3s ease'
+            // });
+        }
+    }
+
+    /**
+     * With windowScroll function,
+     * call fixNavbar(), activateContactLink(), animateProgress()
+     * To help with keeping code more functional
+     */
+    function windowScroll() {
+        fixNavbar();
+        activateContactLink();
+        animateProgress();
+    }
+
+    /**
+     * Invoke windowScroll() function immediately on page load
+     */
     windowScroll();
+    // $('.skills-container').hide();
+    // $('.skills-container').css({
+    //     opacity: 0
+    // });
+
+    // $('.skills-container').show().fadeIn(3000);
+    // $('.skills-container').css('opacity', '0');
+    // $('.skills-container').hide();
+    // $('.skills-container').hide();
+    // $('.skills-container').hide();
+    //.fadeOut(2000);
+    //.css('opacity', '0');
 
     /**
      * When window is scrolled, Call the windowScroll() function to execute
